b221c03b762b5013b8822ea0e1266102b63f64b0
devitcoderz <dev.itcoderz@gmail.com>
2024-12-01 01:18:17 +0500

    b

--- app/Services/ApiService.php
+++ app/Services/ApiService.php
@@ -9,16 +9,14 @@ class ApiService
 {
     public function getData(){
         $response = Http::withHeaders([
-            'Origin' => 'http://bc.game/',
+            'Origin' => 'https://bc.game/',
             'Content-Type' => 'application/json',
         ])
         ->timeout(30) // Set a reasonable timeout in case of issues
-        ->post('http://bc.game/api/agent/open-api/kol/invitees/', [
+        ->post('https://bc.game/api/agent/open-api/kol/invitees/', [
             'invitationCode' => 'sncbc',
             'accessKey' => 'wdDF9rcs17w9JPS6',
         ]);
-
-        dd($response);
 
         // Check the response status or handle errors
         if ($response->successful()) {
